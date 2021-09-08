8681908b458347f3d430e7d8a47fcb6b6a7ad757
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2021-09-09 05:22:02 +0900

    update

--- pico_interval_interrupt.c
+++ pico_interval_interrupt.c
@@ -1,21 +1,31 @@
 #include "pico/stdlib.h"
 
-#define GPIO_00   ( 0 )
+#define GPIO_NO   ( 0 )
+#define LED_PIN (25)
 
-bool timer_callback( repeating_timer_t *rt )
+bool callback(repeating_timer_t *rt)
 {
     /* 割り込み毎にトグルする */
-    if ( gpio_get( GPIO_00 ) != 0 )
+    if ( gpio_get( GPIO_NO ) != 0 )
     {
-        gpio_put( GPIO_00, 0 );
+        gpio_put( GPIO_NO, 0 );
     }
     else
     {
-        gpio_put( GPIO_00, 1 );
+        gpio_put( GPIO_NO, 1 );
     }
 
+    unsigned short hoge=*(unsigned short*)(*rt).user_data;
+    if (hoge==1){
+        gpio_put( LED_PIN, 1 );
+    }
+    else{
+        gpio_put( LED_PIN, 0 );
+    }
+
+
     /* 500us待機 */
-    busy_wait_us_32( 500 );
+    busy_wait_us_32( 50 );
 
     return ( true );
 }
@@ -23,16 +33,28 @@ bool timer_callback( repeating_timer_t *rt )
 void main( void )
 {
     static repeating_timer_t timer;
+    unsigned short hoge=1;
+
 
     /* GPIO設定 */
-    gpio_init( GPIO_00 );
-    gpio_set_dir( GPIO_00, GPIO_OUT );
+    gpio_init( GPIO_NO );
+    gpio_set_dir( GPIO_NO, GPIO_OUT );
+    gpio_init( LED_PIN );
+    gpio_set_dir( LED_PIN, GPIO_OUT );
+
+
 
     /* インターバルタイマ設定 */
-    add_repeating_timer_ms( -1, &timer_callback, NULL, &timer );
+    add_repeating_timer_us( 200, &callback, &hoge, &timer );
 
     while( true )
     {
+        hoge=1;
+        //gpio_put( LED_PIN, 1 );
+        sleep_ms(1000);
+        hoge=0;
+        //gpio_put( LED_PIN, 0 );
+        sleep_ms(1000);
 
     }
 
